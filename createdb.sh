6d8161e367543ed02b4d5daaab360feab01d0909
Brian Glass <bglass@safaribooksonline.com>
2018-01-28 20:26:58 -0700

    Add last table

--- createdb.sh
+++ createdb.sh
@@ -2,3 +2,4 @@ rm oca_calendar.db
 sqlite3 oca_calendar.db < sql/days.sql
 sqlite3 oca_calendar.db < sql/readings.sql
 sqlite3 oca_calendar.db < sql/zachalos.sql
+sqlite3 oca_calendar.db < sql/xceptions.sql
