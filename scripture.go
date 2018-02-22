54bea32cd2822fa5f81551ed89912edd0a6a76a5
Brian Glass <bglass@safaribooksonline.com>
2018-02-22 11:02:47 -0700

    comments

--- scripture.go
+++ scripture.go
@@ -50,30 +50,47 @@ func (self *Bible) Lookup(reference string) Passage {
 }
 
 func (self *Bible) convertReferenceToSQL(reference string) string {
+	/*
+		We parse a scripture reference and convert it to SQL.  A reference
+		includes a book name and a verse specification.  A specification can
+		include multiple verse ranges separated by commas, where a range is a
+		collection of 1 or more contiguous verses. Chapter specification can be
+		implicit. A reference can look like any of the following:
+
+		Matt 1.1-25
+		Matt 4.25-5.13
+		Matt 10.32-36, 11.1
+		Matt 6.31-34, 7.9-11
+		Matt 10.1, 5-8
+	*/
 	var conditionals []string
 	var chapter string
 
-	sql := "select book, chapter, verse, content from bible\n"
+	sql := "select book, chapter, verse, content\nfrom bible\n"
+
+	// Break out book and range specifications
 	groups := regexp.MustCompile(`\s+`).Split(reference, 2)
 	book, specification := groups[0], groups[1]
 
-	sql += fmt.Sprintf("where book = \"%s\" and (\n", book)
+	sql += fmt.Sprintf("where book = \"%s\"\n", book)
 
-	references := regexp.MustCompile(`,\s*`).Split(specification, 4)
-	for _, reference := range references {
-		var conditional string
-		m := refRe.FindStringSubmatch(reference)
+	// Handle each verse range in the specification
+	for _, verseRange := range regexp.MustCompile(`,\s*`).Split(specification, 4) {
+		m := refRe.FindStringSubmatch(verseRange)
 
 		defaultChapter := m[1]
 		if len(defaultChapter) == 0 {
+			// The chapter is implicit, so we use the chapter from the previous range
 			defaultChapter = chapter
 		}
 
+		var conditional string
 		if len(m[4]) > 0 {
-			// Handle ranges
 			if len(m[3]) > 0 && m[3] != m[1] {
+				// Handle ranges that span chapters
 				conditional = fmt.Sprintf(`((chapter = %s and verse >= %s) or (chapter = %s and verse <= %s))`, defaultChapter, m[2], m[3], m[4])
 			} else {
+				// Handle ranges that are contained within a single chapter
 				conditional = fmt.Sprintf(`(chapter = %s and verse between %s and %s)`, defaultChapter, m[2], m[4])
 			}
 		} else {
@@ -89,7 +106,12 @@ func (self *Bible) convertReferenceToSQL(reference string) string {
 		}
 	}
 
-	sql += strings.Join(conditionals, "\nor ") + "\n)\n"
+	if len(conditionals) > 1 {
+		// "Or" all the conditional clauses together
+		sql += "and (\n" + strings.Join(conditionals, "\nor ") + "\n)\n"
+	} else {
+		sql += "and " + conditionals[0] + "\n"
+	}
 
 	return sql
 }
